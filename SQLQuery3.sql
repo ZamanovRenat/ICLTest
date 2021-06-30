afbd21119bb7b4182e48e29ecfc0192162074234
ZamanovRenat <zamanov.renat@mail.ru>
2021-06-30 10:34:59 +0300

    Задание по базе данных

---
+++ SQLQuery3.sql
@@ -0,0 +1,26 @@
+CREATE TABLE Product
+(
+    Id INT PRIMARY KEY IDENTITY,
+    ProductName NVARCHAR(20) NOT NULL,
+    Price MONEY,
+	Description NVARCHAR(MAX)
+);
+
+CREATE TABLE Storage 
+(
+	Id INT PRIMARY KEY IDENTITY,
+	ProductId INT REFERENCES Product (Id), 
+	City NVARCHAR(20),
+	Address NVARCHAR(100)
+);
+
+SELECT * FROM Storage
+WHERE ProductId = VAR
+
+SELECT COUNT(ProductId) FROM Storage WHERE ProductId = VAR
+
+SELECT ProductId FROM Storage WHERE City = VAR
+
+SELECT TOP 1 * FROM Storage
+ORDER BY COUNT(ProductId = VAR) 
+
